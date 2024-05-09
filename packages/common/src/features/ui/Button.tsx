e20179c7af934b23b0bedb128311ca43488f5cdf
keo88 <keo.kim0808@gmail.com>
2024-05-09 22:04:58 +0900

    feat: Add two packages.

---
+++ packages/common/src/features/ui/Button.tsx
@@ -0,0 +1,7 @@
+function Button() {
+  return (
+    <button>This Button</button>
+  );
+}
+
+export default Button;
---
+++ packages/common/src/index.tsx
@@ -0,0 +1,3 @@
+import Button from "./features/ui/Button.tsx";
+
+export default Button;
