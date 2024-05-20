7569728340ccb202d7238855bb3159b2a0a08ade
keo88 <keo.kim0808@gmail.com>
2024-05-20 23:09:39 +0900

    feat: add simple test for counter component.

--- apps/client/__tests__/page.test.tsx
+++ apps/client/__tests__/page.test.tsx
@@ -1,12 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Page from '@/app/page';
+import Counter from '@/components/common/Counter';
 
-describe('Page', () => {
+describe('<Page />', () => {
   it('renders a heading', () => {
     render(<Page />);
 
     const heading = screen.getByRole('heading', { level: 1 });
 
     expect(heading).toBeInTheDocument();
+  });
+
+  it('increments the count', () => {
+    render(<Counter />);
+
+    const button = screen.getByRole('button');
+
+    expect(screen.getByText('0')).toBeInTheDocument();
+
+    fireEvent.click(button);
+
+    expect(screen.getByText('1')).toBeInTheDocument();
   });
 });
