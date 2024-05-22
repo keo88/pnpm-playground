4d193fef8b39988aa438fa5ec1dfaf38ea25b48b
keo88 <keo.kim0808@gmail.com>
2024-05-22 23:51:06 +0900

    fix: fix prettier formatting command on pre commit.

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -3,31 +3,36 @@ import Header from '@/components/common/Header';
 
 export default function Home() {
   return (
-    <div className='flex flex-col h-screen'>
-      <Header/>
+    <div className='flex h-screen flex-col'>
+      <Header />
       <main
-        className='flex-1 overflow-x-hidden overflow-y-auto'
+        className='flex-1 overflow-y-auto overflow-x-hidden'
         style={{
-          perspective: '300px'
-        }}>
-        <div className='bg-purple-600 h-screen flex justify-center items-center'>
+          perspective: '300px',
+        }}
+      >
+        <div className='flex h-screen items-center justify-center bg-purple-600'>
           Intro
-          <Counter/>
+          <Counter />
         </div>
-        <div style={{
-          transformStyle: 'preserve-3d',
-          zIndex: -1
-        }} className='relative h-[80vh]'>
-          <div className='absolute text-white inset-0 flex justify-center items-center'>
-            Front
-          </div>
-          <div style={{
-            transform: 'translate3d(0,0,-300px) scale(2.1)',
-          }} className='h-full flex justify-center items-center text-black bg-fuchsia-200'>
+        <div
+          style={{
+            transformStyle: 'preserve-3d',
+            zIndex: -1,
+          }}
+          className='relative h-[80vh]'
+        >
+          <div className='absolute inset-0 flex items-center justify-center text-white'>Front</div>
+          <div
+            style={{
+              transform: 'translate3d(0,0,-300px) scale(2.1)',
+            }}
+            className='flex h-full items-center justify-center bg-fuchsia-200 text-black'
+          >
             Back
           </div>
         </div>
-        <div className='bg-blue-600 h-screen flex justify-center items-center'>End</div>
+        <div className='flex h-screen items-center justify-center bg-blue-600'>End</div>
       </main>
     </div>
   );
